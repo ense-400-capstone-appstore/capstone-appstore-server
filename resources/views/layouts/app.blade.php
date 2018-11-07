161df7992e2c55e13f22a2d8a50b502d40bce514
Daniel Shevtsov <daniel.shevtsov@gmail.com>
2018-11-06 21:24:11 -0600

    Add includes for CSS, JS in master layout, add CSRF token

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>@lang('app.name') - @yield('title')</title>
 
         <!-- Material Design Lite https://getmdl.io/ -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-        <title>@lang('app.name') - @yield('title')</title>
+        <!-- Compiled application styles -->
+        <link rel="stylesheet" href="css/app.css">
     </head>
     <body>
         <div class="container">
@@ -16,5 +20,7 @@
 
         <!-- Material Design Lite https://getmdl.io/ -->
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        <!-- Compiled application scripts -->
+        <script src="js/app.js"></script>
     </body>
 </html>
