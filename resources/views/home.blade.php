9a3c04040031a823d35a21bf8e61ca98357618ac
Daniel Shevtsov <daniel.shevtsov@gmail.com>
2018-11-06 22:37:32 -0600

    Create MDL layout, home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,67 @@
 
 @section('title', 'Home')
 
-@section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="title m-b-md">
-                Capstone Appstore
-            </div>
+@section('local_styles')
+    <style>
+        h1.app-title {
+            font-size: 64px;
+        }
 
-            <div class="links">
-                <a href="https://github.com/ense-400-capstone-appstore">GitHub</a>
-                <a href="https://ense-400-capstone-appstore.github.io/capstone-appstore-docs/">Docs</a>
-                <a href="/admin">Administration</a>
+        .feature-card {
+            width: 100%;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="mdl-cell mdl-cell--12-col mdl-typography--text-center">
+        <h1 class="mdl-card__title-text center-justified app-title">@lang('app.name')</h1>
+    </div>
+
+    <div class="mdl-cell mdl-cell--4-col">
+        <div class="feature-card mdl-card mdl-shadow--4dp">
+            <div class="mdl-card__title">
+                <h2 class="mdl-card__title-text">Feature 1</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                Placeholder text for feature, this is under construction.
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">
+                Link 1
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="mdl-cell mdl-cell--4-col">
+        <div class="feature-card mdl-card mdl-shadow--4dp">
+            <div class="mdl-card__title">
+                <h2 class="mdl-card__title-text">Feature 2</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                Placeholder text for feature, this is under construction.
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">
+                Link 1
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="mdl-cell mdl-cell--4-col">
+        <div class="feature-card mdl-card mdl-shadow--4dp">
+            <div class="mdl-card__title">
+                <h2 class="mdl-card__title-text">Feature 3</h2>
+            </div>
+            <div class="mdl-card__supporting-text">
+                Placeholder text for feature, this is under construction.
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="#">
+                Link 1
+                </a>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,76 @@
         <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
         <!-- Compiled application styles -->
         <link rel="stylesheet" href="css/app.css">
+        @yield('local_styles')
     </head>
     <body>
-        <div class="container">
-            @yield('content')
+        <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header class="mdl-layout__header">
+                <div class="mdl-layout__header-row">
+                <span class="mdl-layout-title">@lang('app.name')</span>
+                <!-- Add spacer, to align navigation to the right -->
+                <div class="mdl-layout-spacer"></div>
+                <!-- Navigation. We hide it in small screens. -->
+                <nav class="mdl-navigation mdl-layout--large-screen-only">
+                    <a class="mdl-navigation__link" href="https://github.com/ense-400-capstone-appstore">GitHub</a>
+                    <a class="mdl-navigation__link" href="https://ense-400-capstone-appstore.github.io/capstone-appstore-docs/">Docs</a>
+                    <a class="mdl-navigation__link" href="/admin">Administration</a>
+                </nav>
+                </div>
+            </header>
+            <div class="mdl-layout__drawer">
+                <span class="mdl-layout-title">@lang('app.name')</span>
+                <nav class="mdl-navigation">
+                    <a class="mdl-navigation__link" href="https://github.com/ense-400-capstone-appstore">GitHub</a>
+                    <a class="mdl-navigation__link" href="https://ense-400-capstone-appstore.github.io/capstone-appstore-docs/">Docs</a>
+                    <a class="mdl-navigation__link" href="/admin">Administration</a>
+                </nav>
+            </div>
+
+            <main class="mdl-layout__content" style="flex: 1 0 auto;">
+                <div class="page-content">
+                    <div class="mdl-grid">
+                        @yield('content')
+                    </div>
+                </div>
+            </main>
+
+            <footer class="mdl-mega-footer">
+                <div class="mdl-mega-footer__middle-section">
+
+                    <div class="mdl-mega-footer__drop-down-section">
+                    <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+                    <h1 class="mdl-mega-footer__heading">Quick Links</h1>
+                    <ul class="mdl-mega-footer__link-list">
+                        
+                        
+                        
+                        <li><a href="https://github.com/ense-400-capstone-appstore">GitHub</a></li>
+                        <li><a href="https://ense-400-capstone-appstore.github.io/capstone-appstore-docs/">Docs</a></li>
+                        <li><a href="/admin">Administration</a></li>
+                    </ul>
+                    </div>
+
+                </div>
+
+                <div class="mdl-mega-footer__bottom-section">
+                    <div class="mdl-logo">@lang('app.name')</div>
+                    <ul class="mdl-mega-footer__link-list">
+                    <li><a href="https://ense-400-capstone-appstore.github.io/capstone-appstore-docs/help">Help</a></li>
+                    <li><span>&copy; {{date("Y")}}</span></li>
+                    </ul>
+                    
+                    
+                </div>
+            </footer>
         </div>
 
         <!-- Material Design Lite https://getmdl.io/ -->
         <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
         <!-- Compiled application scripts -->
         <script src="js/app.js"></script>
+        @yield('local_scripts')
     </body>
 </html>
+
+
