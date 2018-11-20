4151a80dd6e8f6a0b91b804c6e86b5a9b8711438
oscar l <oscar88feichang6@gmail.com>
2018-11-20 09:32:15 -0600

    disable CategoriesTableSeeder to seed duplicate data; added Category model

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
-
+use App\Category;
 class CategoriesTableSeeder extends Seeder
 {
   /**
@@ -12,19 +12,41 @@ class CategoriesTableSeeder extends Seeder
    */
   public function run()
   {
-
-     DB::table('categories')->updateOrInsert(
-
-      ['name' => "Education"],
-      ['name' => "Business"],
-      ['name' => "Dating"],
-      ['name' => "Entertainment"],
-      ['name' => "Food"],
-      ['name' => "News"],
-      ['name' => "Productivity"],
-      ['name' => "Shopping"],
-      ['name' => "Social"],
-      ['name' => "Weather"]
-    );
+    function CheckCatoryExist($name){
+      $category= Category::firstOrNew(['name' => $name]);
+      return $category;
+    }
+    $categorylist[0]= "Education";
+    $categorylist[1]="Business";
+    $categorylist[2]= "Dating";
+    $categorylist[3]= "Entertainment";
+    $categorylist[4]= "Food";
+    $categorylist[5]= "News";
+    $categorylist[6]= "Productivity";
+    $categorylist[7]= "Shopping";
+    $categorylist[8]= "Social";
+    $categorylist[9]= "Weather";
+    for ($index = 0; $index < count($categorylist); $index++){
+    $obj=CheckCatoryExist($categorylist[$index]);
+    if (!$obj->exists)
+  {
+    $category = new Category;
+    $category->name=$categorylist[$index];
+    $category->save();
+  }
+  }
+    //  DB::table('categories')->updateOrInsert(
+    //
+    //   ['name' => "Education"],
+    //   ['name' => "Business"],
+    //   ['name' => "Dating"],
+    //   ['name' => "Entertainment"],
+    //   ['name' => "Food"],
+    //   ['name' => "News"],
+    //   ['name' => "Productivity"],
+    //   ['name' => "Shopping"],
+    //   ['name' => "Social"],
+    //   ['name' => "Weather"]
+    // );
   }
 }
