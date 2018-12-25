dbca9c46f1275256feb1cd5d58c2083e89362cda
shevtsod <daniel.shevtsov@gmail.com>
2018-12-24 20:37:28 -0600

    Update npm dependencies, add UsersTableSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             CategoriesTableSeeder::class,
             ReviewsTableSeeder::class,
             RepliesTableSeeder::class,
+            UsersTableSeeder::class,
         ]);
     }
 }
