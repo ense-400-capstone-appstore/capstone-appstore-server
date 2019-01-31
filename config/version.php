8552e1a922d3d6affdf7dba320a47c25e1598fc2
shevtsod <daniel.shevtsov@gmail.com>
2019-01-30 22:17:17 -0600

    Disable asset caching across new versions

--- config/version.php
+++ config/version.php
@@ -5,7 +5,7 @@ use Symfony\Component\Process\Process;
 $process = new Process('git describe --tags');
 $process->run();
 
-$currentTag = 'N/A';
+$currentTag = 'unknown-version';
 
 if ($process->isSuccessful()) {
     $currentTag = trim($process->getOutput());
