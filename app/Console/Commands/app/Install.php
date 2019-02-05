94877f6fd0f39377d6d4cb53c6ab89d3a521795c
Daniel Shevtsov <daniel.shevtsov@gmail.com>
2019-02-04 21:43:24 -0600

    Refactor app:install

--- app/Console/Commands/app/Install.php
+++ app/Console/Commands/app/Install.php
@@ -41,61 +41,134 @@ class Install extends Command
     public function handle()
     {
         // Set flags for every option that was passed as input
-        $flagProduction = $this->option('production');
-        $flagCleanInstall = $this->option('clean-install');
-        $flagNoInteraction = $this->option('no-interaction');
+        $this->flagProduction = $this->option('production');
+        $this->flagCleanInstall = $this->option('clean-install');
+        $this->flagNoInteraction = $this->option('no-interaction');
 
-        if ($flagCleanInstall && !$flagNoInteraction) {
-            $confirm = $this->confirm('CAUTION! This will delete ALL application data. Continue?');
+        if (!$this->getConfirmation()) return;
 
-            if (!$confirm) return;
+        // Run installation steps
+        $this->pullCodeChanges();
+        $this->generateAppKey();
+        $this->runMigrations();
+        $this->seedDatabase();
+        $this->linkStorage();
+        $this->installPassport();
+        $this->installFrontEnd();
+
+        $this->showSuccessMessage();
+    }
+
+    /**
+     * Show a warning prompt for users based on certain flags. If the users agree
+     * to the prompt, return true, otherwise false.
+     *
+     * If no warning prompts are shown, return true.
+     *
+     * @return boolean user response
+     */
+    protected function getConfirmation()
+    {
+        if ($this->flagCleanInstall && !$this->flagNoInteraction) {
+            return $this->confirm('CAUTION! This will delete ALL application data. Continue?');
         }
 
-        if ($flagProduction) {
-            echo "Updating application code from GitHub ...\n";
-            exec('git reset --hard');
-            exec('git pull');
-        }
+        return true;
+    }
 
-        if ($flagCleanInstall) {
-            echo "Generating application key ...\n";
-            $this->call('key:generate');
+    /**
+     * Pull newest commit on this branch from GitHub.
+     */
+    protected function pullCodeChanges()
+    {
+        if (!$this->flagProduction) return;
 
-            echo "Dropping database if it exists ...\n";
-        }
+        echo "Updating application code from GitHub ...\n";
+        exec('git reset --hard');
+        exec('git pull');
+    }
 
+    /**
+     * Generate application key for encryption.
+     */
+    protected function generateAppKey()
+    {
+        if (!$this->flagCleanInstall) return;
+
+        echo "Generating application key ...\n";
+        $this->call('key:generate');
+    }
+
+    /**
+     * Run database migrations.
+     */
+    protected function runMigrations()
+    {
         echo "Running migrations ...\n";
-        if ($flagCleanInstall) {
+        if ($this->flagCleanInstall) {
+            echo "Dropping database if it exists ...\n";
             $this->call('migrate:fresh');
-        } elseif ($flagProduction) {
+        } elseif ($this->flagProduction) {
             $this->call('migrate', ['--force' => true]);
         } else {
             $this->call('migrate');
         }
+    }
 
+    /**
+     * Run database seeders.
+     */
+    protected function seedDatabase()
+    {
         echo "Seeding database ...\n";
         $this->call('db:seed');
+    }
 
-        if ($flagCleanInstall) {
-            echo "Linking storage ...\n";
-            $this->call('storage:link');
+    /**
+     * Link the public storage disk.
+     */
+    protected function linkStorage()
+    {
+        if (!$this->flagCleanInstall) return;
 
-            echo "Generating Passport keys ...\n";
-            $this->call('passport:install');
-            $this->call('passport:keys');
-        }
+        echo "Linking storage ...\n";
+        $this->call('storage:link');
+    }
 
+    /**
+     * Install Passport for API authentication.
+     */
+    protected function installPassport()
+    {
+        if (!$this->flagCleanInstall) return;
+
+        echo "Generating Passport keys ...\n";
+        $this->call('passport:install');
+        $this->call('passport:keys');
+    }
+
+    /**
+     * Handle installation of NPM front-end code.
+     */
+    protected function installFrontEnd()
+    {
         echo "Installing front-end dependencies ...\n";
         exec('npm install --no-progress');
 
-        if ($flagProduction) {
+        if ($this->flagProduction) {
             echo "Compiling optimized production front-end code ...\n";
             exec('npm run prod');
         } else {
             echo "Compiling front-end code ...\n";
             exec('npm run dev');
         }
+    }
 
+    /**
+     * Show information to the user after the application installs successfully.
+     */
+    protected function showSuccessMessage()
+    {
         echo "\n";
         echo "Please check above if there were any errors.\n";
         echo "If there were no errors, the application is ready!\n";
