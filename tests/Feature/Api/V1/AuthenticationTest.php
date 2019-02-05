d0e3cf818035f806c3e164765c185222027a2748
Daniel Shevtsov <daniel.shevtsov@gmail.com>
2019-02-04 19:40:25 -0600

    Add tests for authenticating every user role

--- tests/Feature/Api/V1/AuthenticationTest.php
+++ tests/Feature/Api/V1/AuthenticationTest.php
@@ -10,15 +10,37 @@ use App\User;
 class AuthenticationTest extends TestCase
 {
     /**
-     * Assert that the test user can successfully login via the API
+     * Assert that a user can successfully login via the API
+     * @param array $payload User credentials
+     */
+    public function assertUserCanAuthenticate($payload)
+    {
+        $this->json('POST', '/api/v1/login', $payload)
+            ->assertOk()
+            ->assertJsonStructure(['data' => [
+                'user_id',
+                'access_token'
+            ]]);
+    }
+
+    /**
+     * Assert that different types of users can authenticate successfully
      */
     public function testUsersCanAuthenticate()
     {
-        $payload = [
+        $this->assertUserCanAuthenticate([
             'email' => 'user@matryoshkadoll.me',
             'password' => 'password'
-        ];
+        ]);
 
-        $this->json('POST', '/api/v1/login', $payload)->assertStatus(200);
+        $this->assertUserCanAuthenticate([
+            'email' => 'vendor@matryoshkadoll.me',
+            'password' => 'password'
+        ]);
+
+        $this->assertUserCanAuthenticate([
+            'email' => 'admin@matryoshkadoll.me',
+            'password' => 'password'
+        ]);
     }
 }
